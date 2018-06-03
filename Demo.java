dcad1710c05db79957628f98f03e34e687cf0743
nzy189 <songxuchen2015@sian.com>
2018-06-04 00:05:58 +0800

    Squashed commit of the following:
    commit 8f6c9e2d95e79f08a77e2174550269b8c4930eee
    Author: nzy189 <songxuchen2015@sian.com>
    Date:   Mon Jun 4 00:02:25 2018 +0800
    
        及
    
    commit 28235e236c22ee907b1ebc8944986c33a935e43e
    Author: nzy189 <songxuchen2015@sian.com>
    Date:   Mon Jun 4 00:00:36 2018 +0800
    
        查下出租车
    
    commit 81b2cd0ff46eb5e216501880012f550cdcd06d98
    Author: nzy189 <songxuchen2015@sian.com>
    Date:   Sun Jun 3 23:59:45 2018 +0800
    
        分分分
    
    commit 0dbcb1776bd29a6c9a83e69524ccd4936e8904a2
    Author: nzy189 <songxuchen2015@sian.com>
    Date:   Sun Jun 3 23:55:50 2018 +0800
    
        不会就b
    
    commit 7e154cd0c702257de0636acf1c0416acd7217f1e
    Author: nzy189 <songxuchen2015@sian.com>
    Date:   Sun Jun 3 23:54:22 2018 +0800
    
        test
    
    commit c682f80dcea1c5aa3d2f175831d2cd371ce153c9
    Merge: e3b0924 6d0cfcc
    Author: coolsxc03 <33614267+coolsxc03@users.noreply.github.com>
    Date:   Sat Jun 2 21:36:00 2018 +0800
    
        Merge pull request #1 from coolsxc03/readme0edit
    
        Update README.md

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package learningjava;
+
+public class Demo {
+	private static int a;
+public static void main(String[] args){
+	modify(a);
+	System.out.print("hello".equals("hello"));
+
+}
+public static void modify(int a){
+	a++;
+	
+}
+
+
+}
